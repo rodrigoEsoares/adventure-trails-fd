a6aadee077af38d060a95f7c6701697e5a08b55e
Rodrigo Eduardo <rodrigo_e_soares@estudante.sesisenai.org.br>
2024-04-01 17:54:09 -0300

    código final exercicio 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import "./App.css";
 import CardTrilha from "./components/CardTrilha";
 
 function App() {
-  return(
+  const listaTrilhas = [
+    {
+      nomeTrilha: "Trilha da costa da Lagoa",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao: 120,
+      trajeto: 4,
+      dificuldade: "iniciante",
+      tipo: "caminhada/trekking",
+      nomeUsuario: "Guilherme André",
+      urlImagem:
+        "https://images.pexels.com/photos/5036668/pexels-photo-5036668.jpeg?auto=compress&cs=tinysrgb&w=600",
+        // urlImagem: "./assets/imagem-trilha.jpg"
+    },
+  ];
 
-      <CardTrilha />
+  return (
+    <>
+    {
+      listaTrilhas.map((trilha, index)=>(
 
-
-  )
+        <CardTrilha dadosTrilha = {trilha} key={index} />
+      ))
+    }
+    </>
+  );
 }
 
 export default App;
--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,12 +1,26 @@
-function CardTrilha() {
-    
-return(
+import * as PropTypes from "prop-types"
 
-<>
-<h1>Card Trilha</h1>
-</>
-
-)
-
+function CardTrilha({dadosTrilha}) {
+  return (
+    <>
+      <h1>{dadosTrilha.nomeTrilha}</h1>
+      <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+      <img width={200} src={dadosTrilha.urlImagem} alt="imagem trilha"/>
+    </>
+  );
 }
-export default CardTrilha
+CardTrilha.propTypes = {
+    dadosTrilha: PropTypes.exact({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string.isRequired,
+        estado: PropTypes.string.isRequired,
+        duracao: PropTypes.number.isRequired,
+        trajeto: PropTypes.number.isRequired,
+        dificuldade: PropTypes.string.isRequired,
+        tipo: PropTypes.string.isRequired,
+        nomeUsuario: PropTypes.string.isRequired,
+        urlImagem: PropTypes.string.isRequired
+
+    })
+}
+export default CardTrilha;
